dc75f42b673bc2d1bb9b68bad3679a76ede74dd2
Karel63 <76937128+Karel63@users.noreply.github.com>
2021-05-02 16:37:20 +0200

    Add files via upload

---
+++ round_floats.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void generate_items(int *p){
+    pole[] = srand(time(NULL));
+}
+
+void show_items(int *p2){
+    printf("%5.2d ", pole[]);
+}
+
+void round_items(int *p3){
+    pole = (pole / 100) * 100;
+    printf("%5.2d ", pole[]);
+}
+
+int main() {
+    float pole[10];
+    *p = generate_items;
+    *p2 = show_items;
+    *p3 = round_items;
+    }
+    return 0;
+}
